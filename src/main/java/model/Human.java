c1a652861d0facb9fe9306f900cfbc1a5c36abd8
0razzu <0razzu@protonmail.com>
2020-05-13 23:43:20 +0600

    Human

---
+++ src/main/java/model/Human.java
@@ -0,0 +1,95 @@
+package model;
+
+
+import error.JcfErrorCode;
+import error.JcfException;
+
+import java.util.Objects;
+
+
+public class Human {
+    private String firstName;
+    private String patronymicName;
+    private String secondName;
+    private int age;
+    
+    
+    public Human(String firstName, String patronymicName, String secondName, int age) {
+        setFirstName(firstName);
+        setPatronymicName(patronymicName);
+        setSecondName(secondName);
+        setAge(age);
+    }
+    
+    
+    public Human(String firstName, String secondName, int age) {
+        this(firstName, null, secondName, age);
+    }
+    
+    
+    public void setFirstName(String firstName) {
+        if (firstName == null || firstName.length() == 0)
+            throw new JcfException(JcfErrorCode.NULL_FIRST_NAME);
+        
+        this.firstName = firstName;
+    }
+    
+    
+    public void setPatronymicName(String patronymicName) {
+        this.patronymicName = patronymicName;
+    }
+    
+    
+    public void setSecondName(String secondName) {
+        if (secondName == null || secondName.length() == 0)
+            throw new JcfException(JcfErrorCode.NULL_SECOND_NAME);
+        
+        this.secondName = secondName;
+    }
+    
+    
+    public void setAge(int age) {
+        if (age < 0)
+            throw new JcfException(JcfErrorCode.NEGATIVE_AGE);
+        
+        this.age = age;
+    }
+    
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    
+    public String getPatronymicName() {
+        return patronymicName;
+    }
+    
+    
+    public String getSecondName() {
+        return secondName;
+    }
+    
+    
+    public int getAge() {
+        return age;
+    }
+    
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Human)) return false;
+        Human human = (Human) o;
+        return age == human.age &&
+                firstName.equals(human.firstName) &&
+                Objects.equals(patronymicName, human.patronymicName) &&
+                secondName.equals(human.secondName);
+    }
+    
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, patronymicName, secondName, age);
+    }
+}
