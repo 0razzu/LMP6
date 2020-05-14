fc947a5f7842a5eaa7b8f049d5f23c384b2527ff
0razzu <0razzu@protonmail.com>
2020-05-15 00:33:32 +0600

    PhoneBook

--- src/main/java/error/JcfErrorCode.java
+++ src/main/java/error/JcfErrorCode.java
@@ -5,10 +5,15 @@ public enum JcfErrorCode {
     NULL_FIRST_NAME("A first name cannot be null or empty"),
     NULL_SECOND_NAME("A second name cannot be null or empty"),
     NULL_DEPARTMENT("A department name cannot be null or empty"),
-    NEGATIVE_AGE("Age cannot be negative");
+    NULL_PHONE_NUMBER("A phone number cannot be null or empty"),
+    NEGATIVE_AGE("Age cannot be negative"),
+    PERSON_EXISTS("This person is already in the phone book"),
+    PERSON_NOT_FOUND("This person is not in the phone book"),
+    PHONE_NUMBER_EXISTS("This number is already in the phone book"),
+    PHONE_NUMBER_NOT_FOUND("Could not find this phone number");
     
     
-    private String errorString;
+    private final String errorString;
     
     
     JcfErrorCode(String errorString) {
---
+++ src/main/java/model/PhoneBook.java
@@ -0,0 +1,91 @@
+package model;
+
+
+import error.JcfErrorCode;
+import error.JcfException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class PhoneBook {
+    private Map<Human, List<String>> data;
+    
+    
+    public PhoneBook() {
+        data = new HashMap<>();
+    }
+    
+    
+    public void addPerson(Human person) {
+        if (data.containsKey(person))
+            throw new JcfException(JcfErrorCode.PERSON_EXISTS);
+        
+        data.put(person, new ArrayList<>());
+    }
+    
+    
+    public void delPerson(Human person) {
+        if (data.remove(person) == null)
+            throw new JcfException(JcfErrorCode.PERSON_NOT_FOUND);
+    }
+    
+    
+    public void addPhoneNumber(Human person, String phoneNumber) {
+        if (phoneNumber == null || phoneNumber.length() == 0)
+            throw new JcfException(JcfErrorCode.NULL_PHONE_NUMBER);
+        
+        for (List<String> numbers: data.values())
+            if (numbers.contains(phoneNumber))
+                throw new JcfException(JcfErrorCode.PHONE_NUMBER_EXISTS);
+        
+        if (!data.containsKey(person))
+            addPerson(person);
+        
+        else if (data.get(person).contains(phoneNumber))
+            throw new JcfException(JcfErrorCode.PHONE_NUMBER_EXISTS);
+        
+        data.get(person).add(phoneNumber);
+    }
+    
+    
+    public void delPhoneNumber(Human person, String phoneNumber) {
+        if (!data.containsKey(person))
+            throw new JcfException(JcfErrorCode.PERSON_NOT_FOUND);
+        
+        if (!data.get(person).remove(phoneNumber))
+            throw new JcfException(JcfErrorCode.PHONE_NUMBER_NOT_FOUND);
+    }
+    
+    
+    public List<String> getNumbers(Human person) {
+        if (!data.containsKey(person))
+            throw new JcfException(JcfErrorCode.PERSON_NOT_FOUND);
+        
+        return data.get(person);
+    }
+    
+    
+    public Human getPerson(String phoneNumber) {
+        for (Map.Entry<Human, List<String>> entry: data.entrySet())
+            for (String number: entry.getValue())
+                if (number.equals(phoneNumber))
+                    return entry.getKey();
+        
+        throw new JcfException(JcfErrorCode.PERSON_NOT_FOUND);
+    }
+    
+    
+    public PhoneBook getPhoneBookBySecondNamePrefix(String prefix) {
+        PhoneBook phoneBook = new PhoneBook();
+        
+        if (prefix != null && prefix.length() != 0)
+            for (Human person: data.keySet())
+                if (person.getSecondName().startsWith(prefix))
+                    phoneBook.data.put(person, data.get(person));
+        
+        return phoneBook;
+    }
+}
